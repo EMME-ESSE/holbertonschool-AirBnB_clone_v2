f83ddbbda7a1c80ab8173b749800100aae9f6f83
EMME-ESSE <spiritexdi@gmail.com>
2023-03-20 17:37:29 -0300

    Updated

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,13 @@
 -- script that prepares the MySQL server for the project
+
 --creation of the database 
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
 -- creation of the user 
-CREATE USER IF NOT EXISTS "hbnb_dev"@"localhost" IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS "hbnb_test"@"localhost" IDENTIFIED BY 'hbnb_test_pwd';
+
 -- give all privileges to the user on the database
-GRANT ALL PRIVILEGES ON hbnb_dev_db TO "hbnb_dev"@"localhost";
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO "hbnb_test"@"localhost";
+
 -- gives the select privileges to the user
-GRANT SELECT PRIVILEGES ON performance_schema TO "hbnb_dev"@"localhost";
+GRANT SELECT ON performance_schema.* TO "hbnb_test"@"localhost";
