6e90a85e0ad0dff37e915575ff7cd1d436ae7fcd
EMME-ESSE <spiritexdi@gmail.com>
2023-03-21 10:31:57 -0300

    task 4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,23 +1,7 @@
--- SQL test db
+-- Task 4:
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
+IDENTIFIED BY 'hbnb_test_pwd';
 
-import mysql.connector
-
-conn = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="your_password"
-)
-
-cursor = conn.cursor()
-
-cursor.execute("CREATE DATABASE IF NOT EXISTS hbnb_test_db")
-
-cursor.execute("CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'\
-                IDENTIFIED BY 'hbnb_test_pwd'")
-
-cursor.execute("GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost'")
-
-cursor.execute("GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost'")
-
-cursor.close()
-conn.close()
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
