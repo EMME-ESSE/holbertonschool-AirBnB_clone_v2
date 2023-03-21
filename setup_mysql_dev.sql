c6b6e7a7ed04c7c42a2aecdb0ccbe201e82e070b
EMME-ESSE <spiritexdi@gmail.com>
2023-03-21 10:12:56 -0300

    task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,20 +7,26 @@ import sys
 
 if __name__ == "__main__":
 
-db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=hbnb_dev,
-        passwd=hbnb_dev_pwd,
-        db=hbnb_dev_db
-    )
 
-    cursor = db.cursor()
-    cursor.execute("GRANT ALL PRIVILEGES ON hbnb_dev_db \
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=hbnb_dev,
+            passwd=hbnb_dev_pwd,
+            db=hbnb_dev_db
+        )
+
+        cursor = db.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS hbnb_test_db")
+
+        cursor.execute("CREATE USER IF NOT EXISTS "hbnb_test"@"localhost"\
+                        IDENTIFIED BY 'hbnb_test_pwd'")
+
+        cursor.execute("GRANT ALL PRIVILEGES ON hbnb_dev_db \
+                        TO 'hbnb_dev'@'localhost'")
+
+        cursor.execute("GRANT SELECT ON performance_schema \
                     TO 'hbnb_dev'@'localhost'")
 
-    cursor.execute("GRANT SELECT ON performance_schema \
-                    TO 'hbnb_dev'@'localhost'")
-
-    cursor.close()
-    db.close()
+        cursor.close()
+        db.close()
