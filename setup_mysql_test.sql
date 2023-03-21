c9435c59a5d4a6e10b7a09ed1359813d9585d8d8
EMME-ESSE <spiritexdi@gmail.com>
2023-03-21 10:27:52 -0300

    test sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,23 @@
---creation of the database 
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- SQL
 
--- creation of the user 
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+import mysql.connector
 
--- give all privileges to the user on the database
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="your_password"
+)
 
--- gives the select privileges to the user
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+cursor = conn.cursor()
+
+cursor.execute("CREATE DATABASE IF NOT EXISTS hbnb_test_db")
+
+cursor.execute("CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'\
+                IDENTIFIED BY 'hbnb_test_pwd'")
+
+cursor.execute("GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost'")
+
+cursor.execute("GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost'")
+
+cursor.close()
+conn.close()
