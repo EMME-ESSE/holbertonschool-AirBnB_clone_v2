a391ffabf5b8488d86ae93816ceaaab7c5fe9c76
EMME-ESSE <spiritexdi@gmail.com>
2023-03-21 10:23:06 -0300

    test sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,10 +2,10 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- creation of the user 
-CREATE USER IF NOT EXISTS "hbnb_test"@"localhost" IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- give all privileges to the user on the database
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO "hbnb_test"@"localhost";
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- gives the select privileges to the user
-GRANT SELECT ON performance_schema.* TO "hbnb_test"@"localhost";
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
