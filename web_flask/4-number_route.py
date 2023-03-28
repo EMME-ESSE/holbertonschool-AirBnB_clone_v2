993581f7d6a394486041b250c049dd39b577b2c4
EMME-ESSE <spiritexdi@gmail.com>
2023-03-28 20:03:07 -0300

    task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -28,9 +28,11 @@ def taskthree(text="is cool"):
     text = text.replace("_", " ")
     return "Python " + text
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     return '{} is a number'.format(n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
