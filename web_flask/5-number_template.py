21d65d6422e220f9baec8ba8aeef407e02f92cf9
EMME-ESSE <spiritexdi@gmail.com>
2023-03-28 20:47:29 -0300

    Fixing

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def tasktwo(text):
+    t = text.replace("_", " ")
+    return "C" + " " + t
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def taskthree(text="is cool"):
+    text = text.replace("_", " ")
+    return "Python " + text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def temp(n):
+    if isinstance(n, int):
+        return render_template('number.html', number=n)
+    else:
+        return 'Invalid input: n must be an integer'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
