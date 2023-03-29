b92f3a1bb8216b742dfc53fb31535b4991d61eea
EMME-ESSE <spiritexdi@gmail.com>
2023-03-29 17:05:00 -0300

    task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
+"""script that starts a Flask web app"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models import storage
+
 
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """List all states"""   
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda state: state.name)  
     return render_template('7-states_list.html', states=sorted_states)
 
 @app.teardown_appcontext
-def close_state(exception):
-    """Close database connection"""    
+def close_state(exception):   
     storage.close()
 
 if __name__ == '__main__':
