6a52772f79d6160f1f2728e409e4fbf1f4b0af5b
EMME-ESSE <spiritexdi@gmail.com>
2023-03-29 11:52:39 -0300

    task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -40,12 +40,12 @@ def taskfive(n):
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def taskfive(n):
+def tasksix(n):
     if n % 2 == 0:
         result = 'even'
     else:
         result = 'odd'
-    return render_template('6-number_odd_or_even.html', n=n, result=result)
+    return render_template("6-number_odd_or_even.html", n=n, result=result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
