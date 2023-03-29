fe2827428e3d32c2a0a7c11660b34fc806124beb
EMME-ESSE <spiritexdi@gmail.com>
2023-03-29 17:24:27 -0300

    task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python3
-"""Comment"""
+# Hello!
+ #!/usr/bin/python3
+"""script that starts a Flask web app"""
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -8,19 +10,18 @@ from models import storage
 
 app = Flask(__name__)
 
-
+# Commenting
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Comment"""
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda state: state.name)  
     return render_template('7-states_list.html', states=sorted_states)
 
-
+# Comment
 @app.teardown_appcontext
-def close_state(exception):
-    """Comment"""
+def close_state(exception):   
     storage.close()
 
+# Run the Flask app
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
