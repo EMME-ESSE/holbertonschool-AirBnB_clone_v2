e9ee92ebadcbdf84c43dd0046393f9234b3bb6dc
EMME-ESSE <spiritexdi@gmail.com>
2023-03-29 11:44:14 -0300

    Task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -34,7 +34,6 @@ def taskfor(n):
     return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<n>', strict_slashes=False)
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def taskfive(n):
     return render_template("5-number.html", n=n)
