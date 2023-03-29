8455d2ff53f567509ae0878143102806d117654f
EMME-ESSE <spiritexdi@gmail.com>
2023-03-29 16:47:00 -0300

    fixing

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
 from flask import Flask, render_template
-
+from models import storage
+from models.state import State
+from models import storage
 
 app = Flask(__name__)
 
@@ -9,21 +11,14 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """List all states"""
-    
-    from models import storage
-    from models.state import State
-    
+       
     states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    
+    sorted_states = sorted(states, key=lambda state: state.name)  
     return render_template('7-states_list.html', states=sorted_states)
 
 @app.teardown_appcontext
 def close_state(exception):
-    """Close database connection"""
-    
-    from models import storage
-    
+    """Close database connection"""    
     storage.close()
 
 if __name__ == '__main__':
