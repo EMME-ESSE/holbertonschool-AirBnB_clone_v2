d8c49e7b069a5cff105cf56a0ad8815a5e54a5d3
EMME-ESSE <spiritexdi@gmail.com>
2023-04-02 10:59:27 -0300

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
+"""
+Task 5: script that starts a Flask web application
+"""
 from flask import Flask, render_template
-
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
+def hello():
     return "Hello HBNB!"
 
 
@@ -17,26 +18,27 @@ def hbnb():
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def tasktwo(text):
-    t = text.replace("_", " ")
-    return "C" + " " + t
-
-
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def taskthree(text="is cool"):
+def c_is_fun(text):
     text = text.replace("_", " ")
-    return "Python " + text
+    return "C {}".format(text)
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text='is cool'):
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def taskfor(n):
+def print_int(n):
     return '{} is a number'.format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def taskfive(n):
-    return render_template("5-number.html", n=n)
+def displays_html(n):
+    return render_template('5-number.html', n=n)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
